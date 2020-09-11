4e668862666e8e7f6046989a8e7be31a99ca6e4d
WRFitch <WRFitch@outlook.com>
2020-09-11 16:04:31 +0100

    added brtv

--- vidya/vidya.sh
+++ vidya/vidya.sh
@@ -7,11 +7,14 @@ STEAM_APPS_DIR=~/.local/share/Steam/steamapps
 USR_GAMES_DIR=~/Documents/games
 SUPPORTED_GAMES="
 celeste
-quake"
+quake
+Battle Royale TV (brtv)"
 
 show_help(){
 	echo "vidya steam - open steam" 
-	echo "vidya game - run certain videogames. Currently supported titles include: $SUPPORTED_GAMES"
+	echo "vidya game - run certain videogames. Currently supported titles include: $SUPPORTED_GAMES
+
+Game commands are given in brackets if the game title is longer than a word."
 }
 
 select_game(){
@@ -25,6 +28,8 @@ select_game(){
 		quakespasm -basedir $STEAM_APPS_DIR/common/Quake
 	elif [ $GAME == 'celeste' ]; then 
 		$USR_GAMES_DIR/celeste-linux/Celeste
+	elif [ $GAME == "brtv" ]; then 
+		wine $USR_GAMES_DIR/devdiary/brtv/devdiary2.exe
 	else 
 		show_help
 	fi
